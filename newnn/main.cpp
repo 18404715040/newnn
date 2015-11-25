7fac9302be8bb2759cb0ce360e44a65bc0c11934
20141105074 <20141105074qf@mac25.local>
2015-11-25 20:50:52 +0800

    Initial Commit

---
+++ newnn/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  newnn
+//
+//  Created by 20141105074 on 15/11/25.
+//  Copyright © 2015年 20141105074. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
